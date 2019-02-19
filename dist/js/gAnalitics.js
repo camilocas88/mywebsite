64dce024f5adda3f388a6b3458d206647b6bafbd
Camilo Castillo <34112917+camilocas88@users.noreply.github.com>
2019-02-19 11:34:39 -0500

    detalles

--- dist/js/gAnalitics.js
+++ dist/js/gAnalitics.js
@@ -1,7 +1,6 @@
-<script>
+
         window.dataLayer = window.dataLayer || [];
         function gtag() { dataLayer.push(arguments) }
         gtag('js', new Date());
 
         gtag('config', 'UA-121453416-2');
-</script>
